b05f5c0608331aeba4b98f43281dd3bff0c90a9b
Mike McGuire <mike.mcguire@mullenhigh.com>
2022-01-11 14:34:47 -0700

    created files and arrays

---
+++ scaleSelector.js
@@ -0,0 +1,5 @@
+const keys = ["A", "Bb", "B", "C", "C#/Db","D", "D#/Eb","E", "F", "F#/Gb", "G#/Ab"];
+const tonalCenter = ["major", "minor"];
+const modes = ["Ionian", "Dorian", "Phrygian", "Lydian", "Mixolydian", "Aeolian", "Locrian"];
+const fingerings = ["i-m", "i-a", "m-a"];
+const order = ["ascending", "descending"];
